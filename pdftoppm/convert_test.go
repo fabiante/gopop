8db7b7258f7045a27ff7b0f329f265cb0c7f7b4f
fabiante <fabiante@users.noreply.github.com>
2025-06-01 11:55:50 +0200

    Add some options and rough tests to ensure they work.

--- pdftoppm/convert_test.go
+++ pdftoppm/convert_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fabiante/gopop/pdftoppm"
 	"github.com/stretchr/testify/require"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -23,5 +24,84 @@ func TestCommand_Convert(t *testing.T) {
 		files, err := os.ReadDir(dir)
 		require.NoError(t, err)
 		require.Equal(t, 2, len(files), "expected 2 output files, one for each page of the PDF")
+	})
+
+	t.Run("limit to first page", func(t *testing.T) {
+		dir, err := os.MkdirTemp("", "gopop-pdftoppm-test-*")
+		require.NoError(t, err)
+
+		cmd, err := pdftoppm.NewCommand(
+			"test.pdf", dir+"/img",
+			pdftoppm.Last(1),
+		)
+		require.NoError(t, err)
+		require.NotNil(t, cmd)
+
+		err = cmd.Run(context.Background())
+		require.NoError(t, err)
+
+		files, err := os.ReadDir(dir)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(files), "expected 1 output file")
+	})
+
+	t.Run("can define png format", func(t *testing.T) {
+		dir, err := os.MkdirTemp("", "gopop-pdftoppm-test-*")
+		require.NoError(t, err)
+
+		cmd, err := pdftoppm.NewCommand(
+			"test.pdf", dir+"/img",
+			pdftoppm.PNG(),
+		)
+		require.NoError(t, err)
+		require.NotNil(t, cmd)
+
+		err = cmd.Run(context.Background())
+		require.NoError(t, err)
+
+		files, err := os.ReadDir(dir)
+		require.NoError(t, err)
+		require.Equal(t, 2, len(files), "expected 1 output file")
+
+		for _, file := range files {
+			require.True(t, strings.HasSuffix(file.Name(), ".png"), "expected output file %q to be in PNG format", file.Name())
+
+			// Get file info
+			info, err := os.Stat(dir + "/" + file.Name())
+			require.NoError(t, err)
+			size := info.Size()
+
+			// We know that without this limit, pages will be larger that 25K
+			// This is an assertion ment to ensure the test using scale works
+			require.GreaterOrEqual(t, size, int64(25000), "expected output file %q to be small (scale was set to 100)", file.Name())
+		}
+	})
+
+	t.Run("can define max width/height using scale", func(t *testing.T) {
+		dir, err := os.MkdirTemp("", "gopop-pdftoppm-test-*")
+		require.NoError(t, err)
+
+		cmd, err := pdftoppm.NewCommand(
+			"test.pdf", dir+"/img",
+			pdftoppm.ScaleTo(100), // low scale will result in small image size
+		)
+		require.NoError(t, err)
+		require.NotNil(t, cmd)
+
+		err = cmd.Run(context.Background())
+		require.NoError(t, err)
+
+		files, err := os.ReadDir(dir)
+		require.NoError(t, err)
+		require.Equal(t, 2, len(files), "expected 1 output file")
+
+		for _, file := range files {
+			// Get file info
+			info, err := os.Stat(dir + "/" + file.Name())
+			require.NoError(t, err)
+			size := info.Size()
+			// We know that without this limit, pages will be larger that 25K
+			require.Less(t, size, int64(25000), "expected output file %q to be small (scale was set to 100)", file.Name())
+		}
 	})
 }
--- pdftoppm/opts.go
+++ pdftoppm/opts.go
@@ -1,3 +1,41 @@
 package pdftoppm
 
+import "strconv"
+
 type ConvertOption = func(command *Command)
+
+func First(page int) ConvertOption {
+	return func(command *Command) {
+		command.args = append(command.args, "-f", strconv.Itoa(page))
+	}
+}
+
+func Last(page int) ConvertOption {
+	return func(command *Command) {
+		command.args = append(command.args, "-l", strconv.Itoa(page))
+	}
+}
+
+func ScaleTo(scale int) ConvertOption {
+	return func(command *Command) {
+		command.args = append(command.args, "-scale-to", strconv.Itoa(scale))
+	}
+}
+
+func JPG() ConvertOption {
+	return func(command *Command) {
+		command.args = append(command.args, "-jpeg")
+	}
+}
+
+func PNG() ConvertOption {
+	return func(command *Command) {
+		command.args = append(command.args, "-png")
+	}
+}
+
+func TIFF() ConvertOption {
+	return func(command *Command) {
+		command.args = append(command.args, "-tiff")
+	}
+}
